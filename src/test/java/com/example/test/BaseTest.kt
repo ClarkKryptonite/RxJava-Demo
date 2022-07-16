cc6956d8e5533506ce19390cd4e000420fdcee17
kun <kun.chen@qizhidao.com>
2022-07-16 11:10:36 +0800

    Modify(BaseTest): use kotlin file instead of java file

--- src/test/java/com/example/test/BaseTest.kt
+++ src/test/java/com/example/test/BaseTest.kt
@@ -1,44 +1,59 @@
-package com.example.test;
+package com.example.test
 
-import org.junit.jupiter.api.AfterEach;
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.Disposable
+import org.junit.jupiter.api.AfterEach
+import java.util.*
+import java.util.concurrent.TimeUnit
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
-public class BaseTest {
-    private final Random random = new Random();
+open class BaseTest {
+    private val random = Random()
 
     @AfterEach
-    public void interruptMainThread() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(10);
+    @Throws(InterruptedException::class)
+    fun interruptMainThread() {
+        TimeUnit.SECONDS.sleep(10)
     }
 
-    List<String> generateStringList() {
-        return generateStringList(20);
-    }
-
-    List<String> generateStringList(int size) {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            char randomChar = (char) (random.nextInt(26) + 'A');
-            int num = 1 + random.nextInt(10);
-            list.add(generateRepeatChar(randomChar, num));
+    @JvmOverloads
+    fun generateStringList(size: Int = 20): List<String> {
+        val list: MutableList<String> = ArrayList()
+        for (i in 0 until size) {
+            val randomChar = (random.nextInt(26) + 'A'.code).toChar()
+            val num = 1 + random.nextInt(10)
+            list.add(generateRepeatChar(randomChar, num))
         }
-        return list;
+        return list
     }
 
-    private String generateRepeatChar(char c, int repeatCount) {
-        return String.valueOf(c).repeat(Math.max(0, repeatCount));
+    private fun generateRepeatChar(c: Char, repeatCount: Int): String {
+        return c.toString().repeat(Math.max(0, repeatCount))
     }
 
-    List<Integer> generateIntList(int size) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            int randomInt = random.nextInt(100);
-            list.add(randomInt);
+    fun generateIntList(size: Int): List<Int> {
+        val list: MutableList<Int> = ArrayList()
+        for (i in 0 until size) {
+            val randomInt = random.nextInt(100)
+            list.add(randomInt)
         }
-        return list;
+        return list
+    }
+
+    fun <T> Observable<T>.subscribeNext(): Disposable {
+        return subscribe { next: T -> println("onNext:$next") }
+    }
+
+    fun <T> Observable<T>.subscribeNextAndError(): Disposable {
+        return subscribe(
+                { next: T -> println("onNext:$next") }
+        ) { throwable: Throwable -> System.err.println("onError:" + throwable.message) }
+    }
+
+    fun <T> Observable<T>.subscribeAll(): Disposable {
+        return subscribe(
+                { next: T -> println("onNext:$next") },
+                { throwable: Throwable -> System.err.println("onError:" + throwable.message) },
+                { println("onComplete") }
+        )
     }
 }
